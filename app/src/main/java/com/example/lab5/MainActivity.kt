bb0b0914be04b62b073a2c3f6941f307cfc95d56
Hinh12 <121923645+Hinh12@users.noreply.github.com>
2024-05-29 22:43:18 +0700

    Lab5

---
+++ app/src/main/java/com/example/lab5/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.lab5
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.lab5.ui.theme.Lab5Theme
+
+class MainActivity : ComponentActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                val navControl = rememberNavController()
+                NavHost(navController = navControl,
+                    startDestination = "home"
+                ){
+                    composable("home"){
+                        Home(navControl)
+                    }
+                    composable("bai1"){
+                        LoginApp()
+                    }
+                    composable("bai2"){
+                        LightSwitch()
+                    }
+                    composable("bai3"){
+                        CategoryApp()
+                    }
+
+                }
+            }
+        }
+
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab5Theme {
+        Greeting("Android")
+    }
+}
